66b483e2f17dbc1010afe02403fba1431c265ce4
Todd Alexander <60899614+toddalex@users.noreply.github.com>
2020-06-29 17:52:05 -0700

    pushing into staging

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const path = require('path');
-require('dotenv').config()
+// require('dotenv').config()
 
 const app = express();
 
